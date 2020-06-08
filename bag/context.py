e1cc9507a345308db27c63a4a3fe43cc0b2bb653
Juan C. Martin <juankarmartin20@gmail.com>
2020-06-08 13:13:00 +0000

    Finished product size logic

--- bag/context.py
+++ bag/context.py
@@ -11,18 +11,31 @@ def bag_contents(request):
     product_count = 0   # Product count initiates at 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():                   # For eac item in the bag...
-        product = get_object_or_404(Product, pk=item_id)    # ... first, we get the product ...
-        total += quantity * product.price                   # ... then add price of all items to the total ...
-        product_count += quantity                           # ... and number of products to total count
+    for item_id, item_data in bag.items():                      # For each item in the bag...
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)    # ... first, we get the product ...
+            total += item_data * product.price                  # ... then add price of all items to the total ...
+            product_count += item_data                          # ... and number of products to total count
 
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
 
-    if total < settings.FREE_DELIVERY_TRESHOLD: # If total price is lower than the free delivery treshold
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+            })
+
+    if total < settings.FREE_DELIVERY_TRESHOLD:     # If total price is lower than the free delivery treshold
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)   # Calculates delivery amount
         free_delivery_delta = settings.FREE_DELIVERY_TRESHOLD - total   # Shows extra amount neccesary for free delivery
     else:   # If total price is larger than free delivery treshold
