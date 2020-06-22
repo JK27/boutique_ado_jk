f6a5c81e1371aa0fb859cea35edde5639e182948
Juan C. Martin <juankarmartin20@gmail.com>
2020-06-22 11:42:09 +0000

    Updated checkout form and installed django-countries

--- checkout/forms.py
+++ checkout/forms.py
@@ -22,20 +22,20 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True   # Sets autofocus to full name field
         for field in self.fields:
-            if self.fields[field].required:                         # If field is required...
-                placeholder = f'{placeholders[field]} *'            # ... adds a star to placeholder
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder    # Sets placeholder values to dict above
+            if field != 'country':
+                if self.fields[field].required:                         # If field is required...
+                    placeholder = f'{placeholders[field]} *'            # ... adds a star to placeholder
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder    # Sets placeholder values to dict above
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'    # Adds CSS class for styling
             self.fields[field].label = False                                # Removes field labels
--- checkout/models.py
+++ checkout/models.py
@@ -4,27 +4,35 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from products.models import Product
 
 
 # --------------------------------------------------------- ORDER
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False)  # Automatically generated and not editable
+    order_number = models.CharField(max_length=32, null=False,
+                                    editable=False)  # Automatically generated
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)  # Automatically sets order date and time
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    original_bag = models.TextField(null=False, blank=False, default='')    # Contains original bag that created order
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    date = models.DateTimeField(auto_now_add=True)  # Sets order date and time
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,
+                                        null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False,
+                                    default='')  # Bag that created order
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False,
+                                  default='')
 
     # ------------------------------------------- Generate order number method
     def _generate_order_number(self):
