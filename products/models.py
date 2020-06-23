609b28c8837d7f7f38c88be7ca312ea5be36c023
Juan C. Martin <juankarmartin20@gmail.com>
2020-06-23 17:54:41 +0000

    Added edit product functionality

--- products/models.py
+++ products/models.py
@@ -29,7 +29,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2,
                                  null=True, blank=True)
     image_url = models.URLField(max_length=1024, blank=True)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(blank=False)
 
     def __str__(self):
         return self.name
